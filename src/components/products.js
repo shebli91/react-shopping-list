53464277719a8befea4888fa44a0c6e6c67d05de
Mohammad Shebli <mhamadshebli@gmail.com>
2023-05-07 20:32:32 +0300

    - adding the products file - also add  an image to each item

---
+++ src/components/products.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    id: 1,
+    name: "Apple",
+    image: "images/apple.jpg",
+    price: 1.99,
+  },
+  {
+    id: 2,
+    name: "Banana",
+    image: "images/banana.jpg",
+    price: 0.99,
+  },
+  {
+    id: 3,
+    name: "Orange",
+    image: "images/orange.jpg",
+    price: 2.49,
+  },
+  {
+    id: 4,
+    name: "Strawberry",
+    image: "images/strawberry.jpg",
+    price: 3.99,
+  },
+  {
+    id: 5,
+    name: "Blueberry",
+    image: "images/blueberry.jpg",
+    price: 4.49,
+  },
+  {
+    id: 6,
+    name: "Pineapple",
+    image: "images/pineapple.jpg",
+    price: 2.99,
+  },
+  {
+    id: 7,
+    name: "Watermelon",
+    image: "images/watermelon.jpg",
+    price: 6.99,
+  },
+  {
+    id: 8,
+    name: "Grapes",
+    image: "images/grapes.jpg",
+    price: 2.99,
+  },
+  {
+    id: 9,
+    name: "Mango",
+    image: "images/mango.jpg",
+    price: 3.99,
+  },
+  {
+    id: 10,
+    name: "Kiwi",
+    image: "images/kiwi.jpg",
+    price: 1.49,
+  },
+];
+
+export default products;
